adf72a154a53f41e955edc1cf8b6c382a6c6b61c
USER <USER@DESKTOP-QPQS3HF>
2022-03-20 23:20:35 +0900

    by 수진, 2022-03-30 pm11:19 마스터와 코드 맞추어보았지만 병합안됨. 다시 되돌려놓았음.

--- Starbucks/src/main/java/com/team/service/MemberServiceImpl.java
+++ Starbucks/src/main/java/com/team/service/MemberServiceImpl.java
@@ -13,12 +13,12 @@ public class MemberServiceImpl implements MemberService{
 	@Autowired
 	MemberMapper mmapper;
 	
+	/* ----------------------- 회원가입 ----------------------- */
+	
 	// 회원가입 하기 구현
 	@Override
 	public void postJoin(MemberDTO mdto) {
-		System.out.println("서비스1");
 		mmapper.postJoin(mdto);
-		System.out.println("서비스2");
 	}
 	
 	// 회원가입시 아이디 중복체크 구현
@@ -27,6 +27,8 @@ public class MemberServiceImpl implements MemberService{
 		return mmapper.checkId(mdto);
 	}
 
+	/* ----------------------- 로그인 ----------------------- */
+	
 	// 로그인하기 구현
 	@Override
 	public int postLogin(MemberDTO mdto) {
@@ -36,10 +38,45 @@ public class MemberServiceImpl implements MemberService{
 
 	// 로그인 하기 구현(회원정보)
 	@Override
-	public MemberDTO getLogin(MemberDTO mdto) {
-		// TODO Auto-generated method stub
-		return mmapper.getLogin(mdto);
+	public MemberDTO sessionLogin(MemberDTO mdto) {
+		return mmapper.sessionLogin(mdto);
 	}
+	
+	/* ----------------------- 아이디 찾기 ----------------------- */
+	
+		// 본인 인증 구현
+	@Override
+	public int personal_confirm(MemberDTO mdto) {
+		return mmapper.personal_confirm(mdto);
+	}
+	
+	// 아이디 찾기 구현(회원아이디 안내)
+	@Override
+	public MemberDTO findIdCheck(MemberDTO mdto) {
+		return mmapper.findIdCheck(mdto);
+	}
+
+	/* ----------------------- 비밀번호 찾기 ----------------------- */
+	
+	// 아이디 존재 유무 확인 구현
+	@Override
+	public int findPw_userId(MemberDTO mdto) {
+		return mmapper.findPw_userId(mdto);
+	}
+
+	// 이메일 마스킹 처리 구현
+	@Override
+	public String mask_email(MemberDTO mdto) {
+		return mmapper.mask_email(mdto);
+	}
+
+	// 비밀번호 재설정 구현
+	@Override
+	public int resetPw(MemberDTO mdto) {
+		return mmapper.resetPw(mdto);
+	}
+
+
 
 
 
